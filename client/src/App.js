fe39425f0d3bd1e1cf5aa87955cddd06b56b9eb6
Fik0u <fikouch@gmail.com>
2025-04-14 20:53:28 +0100

    NavBar Component

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,17 @@ import Profile from './pages/Profile';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Error from './pages/Error';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
 
+{/* Navbar  */}
+      <NavBar />
+      
       <h1>Glory Games e-Commerce MERN</h1>
-
+{/* Main Routes  */}
       <Routes>
         <Route path = '/' element = { <Home /> } />
         <Route path = '/profile' element = { <Profile /> } />
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../JS/actions/authAction';
+
+
+const NavBar = () => {
+
+    const isAuth = useSelector(state => state.authReducer.isAuth);
+    const dispatch = useDispatch();
+
+  return (
+    <div>
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">
+          {/* Here we add the logo  */}
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            {isAuth ? (
+                <>
+                    <Nav.Link href="/profile">Profile</Nav.Link>
+                    <Nav.Link href="#" onClick = {() => dispatch(logout())}>Logout</Nav.Link>
+
+                </>
+            ) : (
+                <>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/register">Register</Nav.Link>
+                </>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavBar
