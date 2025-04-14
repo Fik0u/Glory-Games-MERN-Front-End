10ba013ed8853dcdb59326fb3d5b60ff46a998f9
Fik0u <fikouch@gmail.com>
2025-04-14 18:13:05 +0100

    First Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Error from './pages/Error';
 
 function App() {
   return (
     <div className="App">
 
       <h1>Glory Games e-Commerce MERN</h1>
+
+      <Routes>
+        <Route path = '/' element = { <Home /> } />
+        <Route path = '/profile' element = { <Profile /> } />
+        <Route path = '/register' element = { <Register /> } />
+        <Route path = '/login' element = { <Login /> } />
+        <Route path = '/*' element = { <Error /> } />
+      </Routes>
       
     </div>
   );
