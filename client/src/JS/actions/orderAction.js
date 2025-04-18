ab19044b1dc5fca3ee08d8047cabe94157697a94
Fik0u <fikouch@gmail.com>
2025-04-18 18:37:19 +0100

    Redux Part III orderAction & orderReducer

---
+++ client/src/JS/actions/orderAction.js
@@ -0,0 +1,63 @@
+// The necessary imports
+import axios from 'axios';
+import { ADD_ORDER, FAIL_ORDER, GET_ALL_ORDERS, GET_MY_ORDERS, GET_ONE_ORDER, LOAD_ORDER, UPDATE_STATUS_ORDER } from "../actionTypes/orderActionTypes";
+
+
+
+//! Action Creators
+
+// Add new order
+export const addOrder = (newOrder, navigate) => async (dispatch) => {
+    dispatch({ type: LOAD_ORDER });
+    try {
+        const result = await axios.post('/api/order/addOrder', newOrder);
+        dispatch({ type: ADD_ORDER, payload: result.data });
+        // navigate('/profile/orders')
+    } catch (error) {
+        dispatch({ type: FAIL_ORDER, payload: error.response.data.errors || error.message })
+    }
+};
+
+// Get User's Orders
+export const getMyOrders = () => async (dispatch) => {
+    dispatch({ type: LOAD_ORDER });
+    try {
+        const result = await axios.get('/api/order/myOrders');
+        dispatch({ type: GET_MY_ORDERS, payload: result.data.orders });
+    } catch (error) {
+        dispatch({ type: FAIL_ORDER, payload: error.response.data.errors || error.message })
+    }
+};
+
+// Get All Orders (Admin Only)
+export const getAllOrders = () => async (dispatch) => {
+    dispatch({ type: LOAD_ORDER });
+    try {
+        const result = await axios.get('/api/order/all');
+        dispatch({ type: GET_ALL_ORDERS, payload: result.data.orders });
+    } catch (error) {
+        dispatch({ type: FAIL_ORDER, payload: error.response.data.errors || error.message })
+    }
+};
+
+// Get One Order
+export const getOneOrder = (id) => async (dispatch) => {
+    dispatch({ type: LOAD_ORDER });
+    try {
+        const result = await axios.get(`/api/order/${id}`);
+        dispatch({ type: GET_ONE_ORDER, payload: result.data.order });
+    } catch (error) {
+        dispatch({ type: FAIL_ORDER, payload: error.response.data.errors || error.message })
+    }
+};
+
+// Update Order Status (admin only)
+export const updateOrderStatus = (id, status) => async (dispatch) => {
+    dispatch({ type: LOAD_ORDER });
+    try {
+        const result = await axios.put(`/api/order/${id}`, { status });
+        dispatch({ type: UPDATE_STATUS_ORDER, payload: result.data.order });
+    } catch (error) {
+        dispatch({ type: FAIL_ORDER, payload: error.response.data.errors || error.message })
+    }
+};
--- client/src/JS/reducers/index.js
+++ client/src/JS/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import prodReducer from './prodReducer';
+import orderReducer from './orderReducer';
 
-const rootReducer = combineReducers({ authReducer, prodReducer });
+const rootReducer = combineReducers({ authReducer, prodReducer, orderReducer });
 
 export default rootReducer;
