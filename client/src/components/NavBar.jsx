24e19daf57a43236c28be471fec4f8ce0fe72bd6
Fik0u <fikouch@gmail.com>
2025-04-21 20:49:08 +0100

    Cart Icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import { logout } from '../JS/actions/authAction';
-
+import { FaShoppingCart } from 'react-icons/fa';
 
 const NavBar = () => {
 
     const isAuth = useSelector(state => state.authReducer.isAuth);
     const user = useSelector(state => state.authReducer.user);
+    const cartItems = useSelector(state => state.cartReducer.cartItems);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const cartItemCount = cartItems ? cartItems.length : 0;
 
   return (
     <div>
@@ -24,7 +28,16 @@ const NavBar = () => {
             {isAuth ? (
                 <>
                     <Nav.Link href="/profile">Profile</Nav.Link>
-                    <Nav.Link href="/cart">Cart</Nav.Link>
+
+                    <Nav.Link href="/cart" className='d-flex align-items-center position-relative'>
+                      <FaShoppingCart size = {24} />
+                      {cartItemCount > 0 && (
+                        <Badge pill bg = 'danger' style = {{position: 'absolute', top: -5, right: -5, fontSize: '0.75rem', width: '16px', height: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0'}}>
+                          {cartItemCount}
+                        </Badge>
+                      )}
+                    </Nav.Link>
+
                     <Nav.Link href="#" onClick = {() => dispatch(logout(navigate))}>Logout</Nav.Link>
 
                 </>
