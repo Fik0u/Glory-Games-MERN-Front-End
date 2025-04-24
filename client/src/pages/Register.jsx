2b6ebb61b65281a1102fd3bd3b59bc43d851f1bd
Fik0u <fikouch@gmail.com>
2025-04-24 21:49:35 +0100

    Register Modif

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -50,29 +50,40 @@ const Register = () => {
       <Form onSubmit = {handleRegister}>
       <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="Enter your full name" name = 'fullName' value = {newUser.fullName} onChange = {handleChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="Enter your username" name = 'username' value = {newUser.username} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="email" placeholder="Enter your email" name = 'email' value = {newUser.email} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="password" placeholder="Enter your password" name = 'password' value = {newUser.password} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="Street" name = 'address.street' value = {newUser.address.street} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="City" name = 'address.city' value = {newUser.address.city} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="Postal Code" name = 'address.postalCode' value = {newUser.address.postalCode} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="text" placeholder="Country" name = 'address.country' value = {newUser.address.country} onChange = {handleChange} />
       </Form.Group>
+
       <Form.Group className="mb-3">
         <Form.Control type="tel" placeholder="Enter your phone number" name = 'phone' value = {newUser.phone} onChange = {handleChange} />
       </Form.Group>
+      
       <p>Already have an account ? <a href="/login">Sign in</a></p>
       <Button variant="primary" type="submit">
         Register
