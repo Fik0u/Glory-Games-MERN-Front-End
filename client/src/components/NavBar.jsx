3f02588053c23ed4e0e05424b3ec695e65fc5b8a
Fik0u <fikouch@gmail.com>
2025-04-25 19:50:38 +0100

    NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import { logout } from '../JS/actions/authAction';
 import { FaShoppingCart } from 'react-icons/fa';
+import '../components/styles/NavBar.css';
 
 
 const NavBar = () => {
@@ -26,10 +27,10 @@ const NavBar = () => {
       const [hovered, setHovered] = useState(false);
 
       const style = {
-        backgroundColor: hovered ? 'rgba(255, 255, 255, 0.1)' : 'transparent',
+        backgroundColor: hovered ? 'rgba(10, 33, 54, 0.64)' : 'transparent',
         color: hovered ? '#0ff' : '#fff',
         textShadow: hovered ? '0 0 5px #0ff' : 'none',
-        transition: 'all 0.3s ease',
+        transition: 'all 0.3s ease-in-out',
       };
       return (
         <NavDropdown.Item
@@ -43,42 +44,47 @@ const NavBar = () => {
       )
     };
 
+
   return (
     <div>
-        <Navbar bg="dark" data-bs-theme="dark" className='d-flex align-items-center'>
+        <Navbar bg="dark" expand="lg" className='py-1' id='navbar'>
         <Container>
-          <Navbar.Brand href="/">
-          {/* Here we add the logo  */} Glory Games
+          <Navbar.Brand href="/" className='d-flex align-items-center'>
+          {/* Here we add the logo  */}
+          <img src="" alt="Glory Games" height="40" className='d-inline-block align-top me-2' />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto me-5">
 
             {/* Products Section  */}
             
             {/* Games Dropdown */}
-            <NavDropdown title="Games" id="games-dropdown" menuVariant="dark" show={showGames} onMouseEnter={() => setShowGames(true)} onMouseLeave={() => setShowGames(false)}>
+            <NavDropdown title={<span className="custom-nav-title">Games</span>} id="games-dropdown" menuVariant="dark" show={showGames} onMouseEnter={() => setShowGames(true)} onMouseLeave={() => setShowGames(false)}>
               <HoverItem href="/category/games/xbox" label="Xbox Games" />
               <HoverItem href="/category/games/playstation" label="PlayStation Games" />
               <HoverItem href="/category/games/nintendo" label="Nintendo Games" />
             </NavDropdown>
 
             {/* Consoles Dropdown */}
-            <NavDropdown title="Consoles" id="consoles-dropdown" menuVariant="dark" show={showConsoles} onMouseEnter={() => setShowConsoles(true)} onMouseLeave={() => setShowConsoles(false)}>
+            <NavDropdown title={<span className="custom-nav-title">Consoles</span>} id="consoles-dropdown" menuVariant="dark" show={showConsoles} onMouseEnter={() => setShowConsoles(true)} onMouseLeave={() => setShowConsoles(false)}>
               <HoverItem href="/category/consoles/xbox" label="Xbox Consoles" />
               <HoverItem href="/category/consoles/playstation" label="PlayStation Consoles" />
               <HoverItem href="/category/consoles/nintendo" label="Nintendo Consoles" />
             </NavDropdown>
 
             {/* Accessories Dropdown */}
-            <NavDropdown title="Accessories" id="accessories-dropdown" menuVariant="dark" show={showAccessories} onMouseEnter={() => setShowAccessories(true)} onMouseLeave={() => setShowAccessories(false)}>
+            <NavDropdown title={<span className="custom-nav-title">Accessories</span>} id="accessories-dropdown" menuVariant="dark" show={showAccessories} onMouseEnter={() => setShowAccessories(true)} onMouseLeave={() => setShowAccessories(false)}>
               <HoverItem href="/category/accessories/xbox" label="Xbox Accessories" />
               <HoverItem href="/category/accessories/playstation" label="PlayStation Accessories" />
               <HoverItem href="/category/accessories/nintendo" label="Nintendo Accessories" />
             </NavDropdown>
+            </Nav>
 
+            <Nav className='ms-auto'>
             {isAuth && !user.isAdmin && (
 
-                    <Nav.Link href="/cart" className='d-flex align-items-center position-relative'>
+                    <Nav.Link href="/cart" className='d-flex align-items-center position-relative me-3'>
                       <FaShoppingCart size = {24} />
                       {cartItemCount > 0 && (
                         <Badge pill bg = 'danger' style = {{position: 'absolute', top: -5, right: -5, fontSize: '0.75rem', width: '16px', height: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0'}}>
@@ -90,7 +96,7 @@ const NavBar = () => {
 {/* Profile Dropdown  */}
             {isAuth && (
                 <NavDropdown title = {
-                      <Image src={user.profilePicture} roundedCircle width={30} height={30} alt='profile' style={{ objectFit: 'cover', border: '2px solid #aaa' }} />
+                      <Image src={user.profilePicture} roundedCircle width={40} height={40} alt='profile' style={{ objectFit: 'cover', border: '2px solid #aaa' }} />
  
                     }
                     id='user-nav-dropdown'
@@ -101,6 +107,7 @@ const NavBar = () => {
                     onMouseLeave = {() => setShowProfileDropdown(false)}
                     style={{ zIndex: 1050 }}
                     className='position-relative'
+                    toggle={false}
                     >
                     
                           <NavDropdown.Item href='/profile'> My Profile</NavDropdown.Item>
@@ -112,7 +119,6 @@ const NavBar = () => {
                     </NavDropdown>
             )}
 
-
             {!isAuth && (
                 <>
                     <Nav.Link href="/login">Login</Nav.Link>
@@ -120,6 +126,7 @@ const NavBar = () => {
                 </>
             )}
           </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
