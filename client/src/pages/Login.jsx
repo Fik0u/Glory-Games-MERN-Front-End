ffe95165e2e4849903e56613fedc0771b393f7f2
Fik0u <fikouch@gmail.com>
2025-04-28 17:01:02 +0100

    Login/Register Modif

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../JS/actions/authAction';
+import { motion } from 'framer-motion';
 
 const Login = () => {
 
@@ -23,105 +24,134 @@ const Login = () => {
     dispatch(login(user, navigate))
   };
 
-
   return (
-    <div style={{
-      maxWidth: '28rem',
-      margin: '0 auto',
-      marginTop: '60px',
-      padding: '2rem',
-      backgroundColor: '#d1d5db',
-      borderRadius: '0.5rem',
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
+    <div style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '80vh',
+      backgroundColor: '#f0f4f8'
     }}>
-      <Form onSubmit={handleLogin}>
-        <Form.Group style={{ marginBottom: '1rem' }}>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter your email" 
-            name='email' 
-            value={user.email} 
-            onChange={handleChange}
-            style={{
-              width: '100%',
-              padding: '0.5rem 0.75rem',
-              border: '1px solid #d1d5db',
-              borderRadius: '0.375rem',
-              outline: 'none',
-              boxSizing: 'border-box'
-            }}
-          />
-        </Form.Group>
-        <Form.Group style={{ marginBottom: '1rem' }}>
-          <Form.Control 
-            type="password" 
-            placeholder="Enter your password" 
-            name='password' 
-            value={user.password} 
-            onChange={handleChange}
-            style={{
-              width: '100%',
-              padding: '0.5rem 0.75rem',
-              border: '1px solid #d1d5db',
-              borderRadius: '0.375rem',
-              outline: 'none',
-              boxSizing: 'border-box'
-            }}
-          />
-        </Form.Group>
-        <p style={{
-          marginTop: '1rem',
-          marginBottom: '1.5rem',
-          color: '#4B5563'
+      <div style={{
+        maxWidth: '28rem',
+        padding: '2rem',
+        backgroundColor: '#fff',
+        borderRadius: '0.5rem',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        width: '100%',
+        marginTop: '1rem',
+      }}>
+
+        <h2 style={{
+          textAlign: 'center', 
+          marginBottom: '40px',
+          fontFamily: 'Arial, sans-serif',
+          fontWeight: 'bold',
+          fontSize: '1.8rem',
+          background: 'linear-gradient(45deg, #1e3a8a, #3b82f6)',
+          backgroundClip: 'text',
+          color: 'transparent', 
+          WebkitBackgroundClip: 'text', 
+          WebkitTextFillColor: 'transparent'
         }}>
-          Don't have an account yet ? 
-          <a href="/register" style={{
-            marginLeft: '0.25rem',
-            color: '#2563EB',
-            fontWeight: '600',
-            textDecoration: 'none'
-          }}
-          onMouseOver={(e) => e.target.style.textDecoration = 'underline'}
-          onMouseOut={(e) => e.target.style.textDecoration = 'none'}
-          >
-            Sign up
-          </a>
-        </p>
-        <button
-      style={{
-        marginTop: "10px",
-        
-        padding: "10px 20px",
-        fontSize: "15px",
-        fontWeight: "bold",
-        background: "linear-gradient(45deg, #041361, #0003c4)",
-        color: "white",
-        border: "none",
-        borderRadius: "8px",
-        cursor: "pointer",
-        transition: "all 0.4s ease",
-        // Ajouts responsive:
-        "@media (max-width: 768px)": {
-          marginLeft: "0",
-          width: "100%",
-          maxWidth: "300px"
-        }
-      }}
-      onMouseOver={(e) => {
-        e.target.style.background = "linear-gradient(45deg,rgb(0, 26, 255),rgb(0, 102, 255))";
-        e.target.style.boxShadow = "0 0 10px #00f2ff, 0 0 20px #00f2ff";
-      }}
-      onMouseOut={(e) => {
-        e.target.style.background = "linear-gradient(45deg, #041361, #0003c4)";
-        e.target.style.boxShadow = "0 0 0px #0003c4";
-      }}
-    >
-      Login
-    </button>
-      </Form>
+          Welcome back, Gamer! Ready to dive back into the action?
+        </h2>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+        >
+          <Form onSubmit={handleLogin}>
+            <Form.Group style={{ marginBottom: '1rem' }}>
+              <Form.Control 
+                type="email" 
+                placeholder="Enter your email" 
+                name='email' 
+                value={user.email} 
+                onChange={handleChange}
+                style={{
+                  width: '100%',
+                  padding: '0.75rem',
+                  border: '1px solid #d1d5db',
+                  borderRadius: '0.375rem',
+                  outline: 'none',
+                  boxSizing: 'border-box',
+                  fontSize: '1rem'
+                }}
+              />
+            </Form.Group>
+
+            <Form.Group style={{ marginBottom: '1rem' }}>
+              <Form.Control 
+                type="password" 
+                placeholder="Enter your password" 
+                name='password' 
+                value={user.password} 
+                onChange={handleChange}
+                style={{
+                  width: '100%',
+                  padding: '0.75rem',
+                  border: '1px solid #d1d5db',
+                  borderRadius: '0.375rem',
+                  outline: 'none',
+                  boxSizing: 'border-box',
+                  fontSize: '1rem'
+                }}
+              />
+            </Form.Group>
+
+            <p style={{
+              marginTop: '1rem',
+              marginBottom: '1.5rem',
+              color: '#4B5563',
+              fontSize: '0.9rem',
+              textAlign: 'center'
+            }}>
+              Don't have an account yet? 
+              <a href="/register" style={{
+                marginLeft: '0.25rem',
+                color: '#2563EB',
+                fontWeight: '600',
+                textDecoration: 'none'
+              }}
+              onMouseOver={(e) => e.target.style.textDecoration = 'underline'}
+              onMouseOut={(e) => e.target.style.textDecoration = 'none'}>
+                Sign up
+              </a>
+            </p>
+
+            <button
+              style={{
+                marginTop: "10px",
+                padding: "10px 20px",
+                fontSize: "15px",
+                fontWeight: "bold",
+                background: "linear-gradient(45deg, #041361, #0003c4)",
+                color: "white",
+                border: "none",
+                borderRadius: "8px",
+                cursor: "pointer",
+                transition: "all 0.4s ease",
+                width: "100%",
+              }}
+              onMouseOver={(e) => {
+                e.target.style.background = "linear-gradient(45deg,rgb(0, 26, 255),rgb(0, 102, 255))";
+                e.target.style.boxShadow = "0 0 10px #00f2ff, 0 0 20px #00f2ff";
+              }}
+              onMouseOut={(e) => {
+                e.target.style.background = "linear-gradient(45deg, #041361, #0003c4)";
+                e.target.style.boxShadow = "0 0 0px #0003c4";
+              }}
+            >
+              Login
+            </button>
+          </Form>
+        </motion.div>
+      </div>
     </div>
-    
   )
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { register } from '../JS/actions/authAction';
+import { motion } from 'framer-motion';
 
 const Register = () => {
-
-  // The state for the new user. It'll be used to store the data entered by the user in the form
   const [newUser, setNewUser] = useState({
     fullName: '',
     username: '',
@@ -29,174 +28,210 @@ const Register = () => {
     if (name.startsWith('address.')) {
       const field = name.split('.')[1];
       setNewUser(prev => ({
-        ...prev, address: { ...prev.address, [field]: value }
-      }))
+        ...prev,
+        address: { ...prev.address, [field]: value }
+      }));
     } else {
-      setNewUser(prev => ({ ...prev, [name]: value }))
+      setNewUser(prev => ({ ...prev, [name]: value }));
     }
   };
 
-
-  //When the user submits the form, this function will send a POST request to the server with the new user's data
   const handleRegister = (e) => {
     e.preventDefault();
-    dispatch(register(newUser, navigate))
+    dispatch(register(newUser, navigate));
   };
-  // console.log(newUser)
 
   return (
-    <div style={{
-      maxWidth: '28rem',
-      margin: '0 auto',
-      marginTop: '60px',
-      marginBottom: "60px",
-      padding: '2rem',
-      backgroundColor: '#d1d5db',
-      borderRadius: '0.5rem',
-      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-    }}>
-  <Form onSubmit={handleRegister}>
-    
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="Enter your full name"
-        name="fullName"
-        value={newUser.fullName}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="Enter your username"
-        name="username"
-        value={newUser.username}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="email"
-        placeholder="Enter your email"
-        name="email"
-        value={newUser.email}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="password"
-        placeholder="Enter your password"
-        name="password"
-        value={newUser.password}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="Street"
-        name="address.street"
-        value={newUser.address.street}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="City"
-        name="address.city"
-        value={newUser.address.city}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="Postal Code"
-        name="address.postalCode"
-        value={newUser.address.postalCode}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="text"
-        placeholder="Country"
-        name="address.country"
-        value={newUser.address.country}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <Form.Group className="mb-4">
-      <Form.Control
-        type="tel"
-        placeholder="Enter your phone number"
-        name="phone"
-        value={newUser.phone}
-        onChange={handleChange}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-      />
-    </Form.Group>
-
-    <p className="mt-4 mb-6 text-gray-600">
-      Already have an account?
-      <a href="/login" className="ml-1 text-blue-600 font-semibold hover:underline">
-        Sign in
-      </a>
-    </p>
-
-    <button
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
       style={{
-        marginTop: "10px",
-        padding: "10px 20px",
-        fontSize: "15px",
-        fontWeight: "bold",
-        background: "linear-gradient(45deg, #041361, #0003c4)",
-        color: "white",
-        border: "none",
-        borderRadius: "8px",
-        cursor: "pointer",
-        transition: "all 0.4s ease",
-        // Ajouts responsive:
-        "@media (max-width: 768px)": {
-          marginLeft: "0",
-          width: "100%",
-          maxWidth: "300px"
-        }
-      }}
-      onMouseOver={(e) => {
-        e.target.style.background = "linear-gradient(45deg,rgb(0, 26, 255),rgb(0, 102, 255))";
-        e.target.style.boxShadow = "0 0 10px #00f2ff, 0 0 20px #00f2ff";
-      }}
-      onMouseOut={(e) => {
-        e.target.style.background = "linear-gradient(45deg, #041361, #0003c4)";
-        e.target.style.boxShadow = "0 0 0px #0003c4";
+        maxWidth: '50rem',
+        margin: '0 auto',
+        marginTop: '60px',
+        marginBottom: "60px",
+        padding: '2rem',
+        backgroundColor: '#d1d5db',
+        borderRadius: '0.5rem',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
       }}
     >
-      Register
-    </button>
-  </Form>
-</div>
 
-  )
-}
+      <h2 style={{
+        textAlign: 'center',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        background: 'linear-gradient(45deg, #2e3b8d, #1c72f2)', 
+        WebkitBackgroundClip: 'text',
+        color: 'transparent',
+        marginBottom: '1.5rem',
+      }}>
+        Welcome to Glory Games! Create your account & start your journey with us.
+      </h2>
 
-export default Register
+      <Form onSubmit={handleRegister}>
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: '20px',
+          maxWidth: '100%',
+        }}
+          className="form-grid"
+        >
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="Full Name"
+              name="fullName"
+              value={newUser.fullName}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={newUser.username}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={newUser.email}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={newUser.password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="Street"
+              name="address.street"
+              value={newUser.address.street}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="City"
+              name="address.city"
+              value={newUser.address.city}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="Postal Code"
+              name="address.postalCode"
+              value={newUser.address.postalCode}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="input-custom"
+              type="text"
+              placeholder="Country"
+              name="address.country"
+              value={newUser.address.country}
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Form.Group style={{ gridColumn: '1 / 3' }}>
+            <Form.Control
+              className="input-custom"
+              type="tel"
+              placeholder="Phone Number"
+              name="phone"
+              value={newUser.phone}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </div>
+
+        <p className="mt-4 mb-6 text-gray-600">
+          Already have an account?
+          <a href="/login" className="ml-1 text-blue-600 font-semibold hover:underline">
+            Sign in
+          </a>
+        </p>
+
+        <button
+          style={{
+            marginTop: "10px",
+            padding: "10px 20px",
+            fontSize: "15px",
+            fontWeight: "bold",
+            background: "linear-gradient(45deg, #041361, #0003c4)",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            cursor: "pointer",
+            transition: "all 0.4s ease",
+          }}
+          onMouseOver={(e) => {
+            e.target.style.background = "linear-gradient(45deg,rgb(0, 26, 255),rgb(0, 102, 255))";
+            e.target.style.boxShadow = "0 0 10px #00f2ff, 0 0 20px #00f2ff";
+          }}
+          onMouseOut={(e) => {
+            e.target.style.background = "linear-gradient(45deg, #041361, #0003c4)";
+            e.target.style.boxShadow = "0 0 0px #0003c4";
+          }}
+        >
+          Register
+        </button>
+      </Form>
+
+
+      <style jsx="true">{`
+        @media (max-width: 768px) {
+          .form-grid {
+            grid-template-columns: 1fr !important;
+          }
+        }
+
+        .input-custom {
+          transition: box-shadow 0.3s ease, border-color 0.3s ease;
+        }
+
+        .input-custom:focus {
+          border-color: #3b82f6;
+          box-shadow: 0 0 8px #3b82f6;
+          outline: none;
+        }
+      `}</style>
+    </motion.div>
+  );
+};
+
+export default Register;
