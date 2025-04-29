4bece323bc4ab8fabc27e5c861fb946d8cca6d03
Fik0u <fikouch@gmail.com>
2025-04-29 21:31:05 +0100

    Profil Fix

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -100,7 +100,7 @@ const Profile = () => {
           flex: '0 1 45%', 
           paddingRight: '5px', 
         }}>
-          <h3 style={{ color: '#fff', marginBottom: '20px' }}> 👋 Hello {user.username} </h3>
+          <h3 style={{ color: '#fff', marginBottom: '20px', marginLeft: '30px' }}> 👋 Hello {user.username} </h3>
           <img 
             src={user.profilePicture || "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"} 
             alt="profile" 
